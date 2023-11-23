694e458ccd2323702d701c6269635936dd3a80c1
adhiml <122215755+adhiml@users.noreply.github.com>
2023-11-23 11:17:21 +0800

    Update L5Q2.java
    Have to look back

--- Lab5/src/L5Q2.java
+++ Lab5/src/L5Q2.java
@@ -14,6 +14,13 @@ public class L5Q2 {
         Set <Integer> takulang = new HashSet <> ();
         int [] random = new int[10];
 
+// need to initialize to 0 so 0 can be generated
+        
+        for (int i = 0; i < 10 ; i++)
+        {
+            random [i] = 0;
+        }
+
         for (int i = 0; i < 10 ; i++)
         {
             random[i] = g.nextInt(21);
@@ -22,6 +29,22 @@ public class L5Q2 {
                 {
                     System.out.print(random[i] + " ");
                 } 
+        }
+
+        for (int i = 0; i < 10 ; i++)
+        {
+            random[i] = g.nextInt(21);
+            
+            for (int n = 0; n < 10;n++)
+                {
+                    if (random[n] == random[i]);
+                        break;
+                    if (random[n] != random[i]);
+                        return random[n];
+                } 
+
+                System.out.print(random[i] + " ");
+
         }
     }
 }
